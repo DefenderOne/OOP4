a76bf27300fe70ffed19beaa532f690679f5ac78
DefenderOne <DefenderOne@users.noreply.github.com>
2020-10-15 09:33:05 +0300

    Added DeviceModel class definition

--- Headers/Good.h
+++ Headers/Good.h
@@ -15,6 +15,8 @@ class GoodModel {
 
     bool GetPrice();
     bool GetName();
+
+    virtual std::string GetInfo() = 0;
 };
 
 // Food model derived from good model
@@ -25,21 +27,26 @@ class FoodModel : public GoodModel {
     int _carbohydrateLevel;
     int _proteinLevel;
     public:
-    FoodModel(std::string name, int value, double weight, int calories, int fatLevel, int carbohydrateLevel, int proteinLevel);
+    FoodModel(std::string name, int price, double weight, int calories, int fatLevel, int carbohydrateLevel, int proteinLevel);
     std::string GetNutritionInfo();
+    std::string GetInfo() override;
 };
 
 
 class DeviceModel : public GoodModel {
-
+    private:
+    bool _isAutonomous;
+    int _energyConsumption;
+    std::string _usage;
+    
+    public:
+    DeviceModel(std::string name, int price, double weight, bool isAutonomous, int energyConsumption, std::string usage);
+    std::string GetPowerUnitInfo();
+    std::string GetInfo() override;
 };
 
 // Chemistry model derived from good model
 class ChemistryModel : public GoodModel {
-
-};
-
-class ToolModel : public GoodModel {
 
 };
 
